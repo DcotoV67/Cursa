f083ece2d9b188f63aa87918840d0fff9caca335
alexen999 <alexpf49@gmail.com>
2019-10-23 19:49:31 +0200

    asdasfa3qew4

--- src/com/cursa/Main.java
+++ src/com/cursa/Main.java
@@ -17,7 +17,8 @@ public class Main {
 
             System.out.println("ESPAÑOLES, he salido");
         }else{
-            System.out.println("Era una broma, o quizá no...");        }
-
+            System.out.println("Era una broma, o quizá no...");
+            System.out.println("TE QUIERO NENE");
+        }
     }
 }
