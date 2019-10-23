dcd412fc911a7eb7dd69d7425b36e13e1aab95de
dcotov67 <dcotov67@elpuig.xeill.net>
2019-10-23 19:11:23 +0200

    franco

--- src/com/cursa/Main.java
+++ src/com/cursa/Main.java
@@ -3,5 +3,5 @@ package com.cursa;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Mañana es jueves y se sale");    }
+        System.out.println("Franco: Mañana es jueves y se sale");    }
 }
