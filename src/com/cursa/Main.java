0571797b852ffa380fde4649d026dfc6bb5d8430
dcotov67 <dcotov67@elpuig.xeill.net>
2019-10-23 19:43:29 +0200

    franco

--- src/com/cursa/Main.java
+++ src/com/cursa/Main.java
@@ -1,12 +1,23 @@
 package com.cursa;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Franco: Mañana es jueves y se sale");
         int gvhj;
 
+        Scanner scanner = new Scanner(System.in);
+        String franco = "";
 
+        franco = scanner.nextLine();
+
+        if (franco.equals("fuera")){
+
+            System.out.println("ESPAÑOLES, he salido");
+        }else{
+            System.out.println("Era una broma");        }
 
     }
 }
