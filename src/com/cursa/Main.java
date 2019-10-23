ce69fc487ab9a6b5744f1c6709134a4b6be8f5e0
alexen999 <alexpf49@gmail.com>
2019-10-23 19:30:23 +0200

    hola

--- src/com/cursa/Main.java
+++ src/com/cursa/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Franco: Mañana es jueves y se sale");
-        int gvhju;
+        int gvhj;
 
     }
 }
