a8ff70748fedbbd00183901e9fab63480ac98f78
dcotov67 <dcotov67@elpuig.xeill.net>
2019-10-29 17:11:51 +0100

    menu

--- src/com/cursa/Vehiculo.java
+++ src/com/cursa/Vehiculo.java
@@ -6,32 +6,17 @@ public class Vehiculo {
     private String coche = "coche";
     private String camion = "camión";
 
-    public void setBicicleta(String bicicleta) {
-        this.bicicleta = bicicleta;
-    }
 
     public String getBicicleta() {
         return bicicleta;
-    }
-
-    public void setCamion(String camion) {
-        this.camion = camion;
     }
 
     public String getCamion() {
         return camion;
     }
 
-    public void setCoche(String coche) {
-        this.coche = coche;
-    }
-
     public String getCoche() {
         return coche;
-    }
-
-    public void setMoto(String moto) {
-        this.moto = moto;
     }
 
     public String getMoto() {
